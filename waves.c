77732cbb3987acdde3765c8072ef8083118d5ec9
Horacio Zerpa <eduzerpa115@gmail.com>
2025-11-25 23:25:05 -0500

    Switches conditional

--- waves.c
+++ waves.c
@@ -10,6 +10,7 @@ volatile unsigned int *adc = (unsigned int *) ADC_ADDR;
 volatile unsigned int *dac = (unsigned int *) DAC_ADDR;
 volatile unsigned int *led = (unsigned int *) LED_ADDR;
 volatile unsigned int *sws = (unsigned int *) SWS_ADDR;
+
 void mostrar_voltaje(float voltaje){
 	unsigned int entero = (unsigned int) voltaje;
 	unsigned int decimas = (unsigned int) ((voltaje - entero)*10)%10;
@@ -18,16 +19,17 @@ void mostrar_voltaje(float voltaje){
 	unsigned int display = (entero << 12) | (decimas << 8) | (centesimas << 4) | milesimas;
 	*led = display;
 }
+
 int main()
 {
     printf("Hello from Nios II!\n");
     printf("EE604 Introduccion a Microcontroladores - 4to Laboratorio 2025-1\n");
     
-    unsigned int valor_adc=0;
-    unsigned int valor_switch=0;
-    float voltaje =0;
+    unsigned int valor_adc = 0;
+    unsigned int valor_switch = 0;
+    float voltaje = 0;
     float array[4096];
-    unsigned int valor_dac=0;
+    unsigned int valor_dac = 0;
     int flag=0;
     while (1) {
     	int i,j;
@@ -35,34 +37,34 @@ int main()
 		voltaje= (valor_adc*4.096)/4095.0;
 		if( flag == 0) {
 		valor_switch= *sws;	
-		for( i=0; i< 4096; i++){
+		for( i = 0; i < 4096; i++){
 			float tiempo = (float) i /4095; // va de 0 a 1
-			float onda =0.0;
-			if (valor_switch >= 0x10){
+			float onda = 0.0;
+			if(valor_swtich > 0x1F && valor_switch < 0x3F){
 				if (tiempo <= 0.5 ){
 						onda = tiempo;
-						array[i]=onda;
+						array[i] = onda;
 					}else{
 						onda = 1.0 - tiempo;
-						array[i]=onda;
+						array[i] = onda;
 					}
 			}
 			else{
 				if(voltaje < 2.5){
 					if (tiempo <= 0.5 ){
 						onda = tiempo;
-						array[i]=onda;
+						array[i] = onda;
 					}else{
 						onda = 1.0 - (tiempo * tiempo);
-						array[i]=onda;
+						array[i] = onda;
 					}
 				}else{
 					if (tiempo <= 0.5 ){
 						onda = tiempo * tiempo;	
-						array[i]=onda;
+						array[i] = onda;
 					}else{
 						onda = (1.0 - tiempo);
-						array[i]=onda;
+						array[i] = onda;
 					}
 				}	
 			}
@@ -71,15 +73,15 @@ int main()
 			if ((i%200) == 0){
 				mostrar_voltaje(voltaje);
 			}
-			if(i == 4095) flag = 1;
+			if(i == 4095)flag = 1;
 			}
 		}else{
 			for(j=0;j<4096;j++){
 				valor_dac = (unsigned int)(array[j]*4095);
-				*dac= valor_dac;
+				*dac = valor_dac;
 			}
 			if(valor_switch != *sws){
-				flag=0;
+				flag = 0;
 			}
 			
 		}
